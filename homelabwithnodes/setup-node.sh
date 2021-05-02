a0b88c60491aec8c9d4e2b1f94802c117223edac
wlanboy <me@wlanboy.com>
2021-05-02 07:08:31 +0200

    added nodename as parameter

--- homelabwithnodes/setup-node.sh
+++ homelabwithnodes/setup-node.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-sudo kubeadm join ${CONTROL_IP}:6443 --token ${TOKEN} --discovery-token-unsafe-skip-ca-verification --v=4
+sudo kubeadm join ${CONTROL_IP}:6443 --token ${TOKEN} --discovery-token-unsafe-skip-ca-verification --node-name ${NODE_NAME} --v=2
