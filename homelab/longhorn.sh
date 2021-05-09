88a5d559290257a17948fed959fb5c80afb83df8
wlanboy <me@wlanboy.com>
2021-05-09 16:48:27 +0200

    added longhorn storage provider to vm

---
+++ homelab/longhorn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+# setup longhorn
+kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.1.1/deploy/longhorn.yaml
+# create storage class
+#kubectl create -f longhorn-storage-class.yml
+sleep 5
+kubectl apply -f longhorn-ingress.yml
