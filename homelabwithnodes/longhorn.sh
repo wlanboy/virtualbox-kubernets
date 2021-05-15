d40664ccfca4ac4c1a39f204c7dc3a91d06b622e
wlanboy <me@wlanboy.com>
2021-05-15 07:54:42 +0200

    added comments to longhorn scripts

--- homelabwithnodes/longhorn.sh
+++ homelabwithnodes/longhorn.sh
@@ -5,5 +5,5 @@ export KUBECONFIG=/etc/kubernetes/admin.conf
 # setup longhorn
 kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.1.1/deploy/longhorn.yaml
 # create storage class
-#kubectl create -f longhorn-storage-class.yml
-#kubectl apply -f longhorn-ingress.yml
+#kubectl create -f longhorn-storage-class.yml #now included
+#kubectl apply -f longhorn-ingress.yml # if you wish to access to longhorn web frontend (does currently not support relative paths)
